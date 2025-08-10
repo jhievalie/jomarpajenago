dc5e030f5448e8b10c03bb75a59626b760467136
jhievalie <38378249+jhievalie@users.noreply.github.com>
2025-08-10 01:07:18 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Span small-text
-const titles = ["Data Entry Specialist", "Data Analyst", "Reports Specialist", "Research Analyst"];
+const titles = ["Data Analyst", "Data Entry Specialist", "Reports Specialist", "Research Analyst", "Data Annotation"];
 let titleIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
